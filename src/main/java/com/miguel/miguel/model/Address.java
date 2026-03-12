7ac1f3c58c0775b715b66414f3e6d6f933590dd9
Miguel Matus <uribematusma@gmail.com>
2026-03-12 10:40:57 -0600

    Creating models User and Address

---
+++ src/main/java/com/miguel/miguel/model/Address.java
@@ -0,0 +1,27 @@
+package com.miguel.miguel.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String street;
+    private String country_code;
+
+    @ManyToOne
+    private User user;
+
+}
---
+++ src/main/java/com/miguel/miguel/model/User.java
@@ -0,0 +1,29 @@
+package com.miguel.miguel.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue (strategy = GenerationType.UUID)
+    private UUID id;
+    private String email;
+    private String name;
+    private String phone;
+    private String password;
+    private String tax_id;
+
+    @ManyToOne
+    private Address addresses;
+
+}
